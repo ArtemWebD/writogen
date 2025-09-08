f8682341406cc570de85ff2d4362c3d05c22773b
artemWebD <artemtoptun@gmail.com>
2025-09-08 09:37:30 +0700

    ref #00008 created cart components

--- src/components/product-section/ProductSection.tsx
+++ src/components/product-section/ProductSection.tsx
@@ -1,10 +1,15 @@
 import { Box, Container, Grid } from "@mui/material";
-import { memo } from "react";
+import { memo, type FC } from "react";
 import BaseH2 from "../base/base-title/BaseH2";
 import BaseH3 from "../base/base-title/BaseH3";
 import ProductCard from "./ProductCard";
+import type { IProduct } from "../../App";
 
-const ProductSection = () => {
+interface IProps {
+  onAddProduct?: (product: IProduct) => void;
+}
+
+const ProductSection: FC<IProps> = ({ onAddProduct }) => {
   return (
     <Box className="bg-white-color pt-[6rem] pb-[6rem]">
       <Container className="!flex flex-col justify-center items-center">
@@ -24,30 +29,38 @@ const ProductSection = () => {
           className="!mt-[4rem]"
         >
           <ProductCard
+            id={1}
             title="Мобильные приложения"
             imageUrl="/images/products/1.webp"
             price={3000}
             description="Которые сократят расходы в производственных процессах и увеличат лояльность конечных покупателей"
+            onAddProduct={onAddProduct}
           />
           <ProductCard
+            id={2}
             title="Корпоративные приложения"
             imageUrl="/images/products/2.webp"
             price={5000}
             description="1С, SAP, самописные системы - всё на одном экране. Создаем единую внутреннюю среду в компаниях"
+            onAddProduct={onAddProduct}
             animationDelay={0.15}
           />
           <ProductCard
+            id={3}
             title="AR и VR приложения"
             imageUrl="/images/products/3.webp"
             price={4000}
             description="Для диагностики и управления промышленным оборудованием, адресного хранения на складах"
+            onAddProduct={onAddProduct}
             animationDelay={0.3}
           />
           <ProductCard
+            id={4}
             title="Индивидуальная разработка"
             imageUrl="/images/products/4.webp"
             price={1000}
             description="Готовы создать программное обеспечение для различного вида устройств"
+            onAddProduct={onAddProduct}
             animationDelay={0.45}
           />
         </Grid>
--- src/main.tsx
+++ src/main.tsx
@@ -1,32 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Header from "./components/header/Header";
 import "./assets/index.css";
-import FirstScreen from "./components/first-screen/FirstScreen";
-import { motion } from "framer-motion";
-import { GLOBAL_ANIMATION_DELAY, GLOBAL_ANIMATION_DURATION } from "./const";
-import ProductSection from "./components/product-section/ProductSection";
-import WorkSection from "./components/work-section/WorkSection";
-import ContactsSection from "./components/contacts-section/ContactsSection";
-import Footer from "./components/footer/Footer";
+import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <motion.div
-      key={"root"}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{
-        duration: GLOBAL_ANIMATION_DURATION,
-        delay: GLOBAL_ANIMATION_DELAY,
-      }}
-    >
-      <Header />
-      <FirstScreen />
-      <ProductSection />
-      <WorkSection />
-      <ContactsSection />
-      <Footer />
-    </motion.div>
+    <App />
   </StrictMode>
 );
