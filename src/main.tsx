246bd64c6f3916b22fc61d39fe1a95a5199c0a48
artemWebD <artemtoptun@gmail.com>
2025-09-08 07:58:25 +0700

    ref #00007 created footer

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import { GLOBAL_ANIMATION_DELAY, GLOBAL_ANIMATION_DURATION } from "./const";
 import ProductSection from "./components/product-section/ProductSection";
 import WorkSection from "./components/work-section/WorkSection";
 import ContactsSection from "./components/contacts-section/ContactsSection";
+import Footer from "./components/footer/Footer";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -25,6 +26,7 @@ createRoot(document.getElementById("root")!).render(
       <ProductSection />
       <WorkSection />
       <ContactsSection />
+      <Footer />
     </motion.div>
   </StrictMode>
 );
